1c079ec8b9467e7628af9acbca3a1117a4a0e3f8
Yareh <cha15080@byui.edu>
2021-06-17 11:14:55 -0600

    Changes made

--- src/chavez/HelloFromMinhah.java
+++ src/chavez/HelloFromMinhah.java
@@ -4,5 +4,6 @@ public class HelloFromMinhah {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Minhah!");
+        System.out.println("How are you?");
     }
 }
